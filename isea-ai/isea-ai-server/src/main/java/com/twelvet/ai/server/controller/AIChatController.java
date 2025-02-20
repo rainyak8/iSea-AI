8d614bebc120d5b8dfd9b989e92d78c77c075c87
Liu zhijian <1965817202@qq.com>
2025-02-21 00:57:30 +0800

    feat(AI): 完善对话接口

--- isea-ai/isea-ai-server/src/main/java/com/twelvet/ai/server/controller/AIChatController.java
+++ isea-ai/isea-ai-server/src/main/java/com/twelvet/ai/server/controller/AIChatController.java
@@ -1,9 +1,13 @@
 package com.twelvet.ai.server.controller;
 
+import cn.dev33.satoken.annotation.SaIgnore;
 import com.twelvet.ai.server.service.DifyAIChatService;
 import com.twelvet.framework.core.application.controller.TWTController;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.Resource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
@@ -17,16 +21,18 @@ import reactor.core.publisher.Flux;
 @RequestMapping("/ai/chat")
 public class AIChatController extends TWTController {
 
-	@Resource
-	private DifyAIChatService agent;
-
-	@RequestMapping(path="/simpleChat")
+	@Autowired
+	private DifyAIChatService difyAgent;
+	@Operation(summary = "普通对话")
+	@SaIgnore
+	@GetMapping(path="/simpleChat")
 	public String simpleChat(String chatId, String userMessage) {
-		return agent.simpleChat(chatId, userMessage);
+		return difyAgent.simpleChat(chatId, userMessage);
 	}
-
-	@RequestMapping(path="/streamChat")
+	@Operation(summary = "流式对话")
+	@SaIgnore
+	@GetMapping(path="/streamChat")
 	public Flux<String> streamChat(String chatId, String userMessage) {
-		return agent.streamChat(chatId, userMessage);
+		return difyAgent.streamChat(chatId, userMessage);
 	}
 }
